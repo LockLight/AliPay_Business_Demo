42163c99c6056f6151cd31e238de4717c37ab459
LockLight <Locklightz@gmail.com>
2017-01-04 19:08:02 +0800

    Initial Commit

---
+++ AliPay-口碑页面Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AliPay-口碑页面Demo
+//
+//  Created by locklight on 17/1/4.
+//  Copyright © 2017年 LockLight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AliPay-口碑页面Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AliPay-口碑页面Demo
+//
+//  Created by locklight on 17/1/4.
+//  Copyright © 2017年 LockLight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
